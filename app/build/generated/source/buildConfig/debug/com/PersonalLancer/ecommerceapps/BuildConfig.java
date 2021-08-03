8ac2af1bd3c42c3f7c6b460b9b1ac883ee61df54
indriyanto Nugroho <indriyantongrhh@gmail.com>
2021-08-03 11:14:48 +0700

    add build config

---
+++ app/build/generated/source/buildConfig/debug/com/PersonalLancer/ecommerceapps/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.PersonalLancer.ecommerceapps;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.PersonalLancer.ecommerceapps";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.1";
+
+  // Field from default config.
+  public static final String BASE_URL = "https://uts.jualrumahsemarang.com/ecommers/";
+}
